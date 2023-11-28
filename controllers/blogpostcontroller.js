86ab5ee87e24f1d0b073de02c9276cd63117eb3c
Pravin Nichal <nichal.pravin2001@gmail.com>
2023-11-28 21:14:37 +0530

    commit

--- controllers/blogpostcontroller.js
+++ controllers/blogpostcontroller.js
@@ -1,3 +1,10 @@
+import {
+  deleteBlogPostService,
+  getAllBlogPostsService,
+  getBlogPostByIdService,
+  saveBlogPostService,
+  updateBlogPostService,
+} from "../services/postservices.js";
 import fs from "fs";
 import { fileURLToPath } from "url";
 import path, { dirname } from "path";
@@ -6,34 +13,24 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 var filepath = path.join(__dirname, "..", "jsondatastore/blogpost.json");
 
-export const getAllBlogPost = (req, res) => {
-  fs.readFile(filepath, "utf8", function (err, data) {
-    if (err && err.code == "ENOENT") {
+export const getAllBlogPost = async (req, res) => {
+  await getAllBlogPostsService().then((blogPost) => {
+    if (blogPost == undefined) {
       return res
         .status(404)
         .json({ status: 404, message: "No Posts Found", data: {} });
     }
-    const jsonData = JSON.parse(data);
-    res.status(201).json({
+    return res.status(201).json({
       status: 201,
       message: "Posts Fetched Successfully",
-      data: jsonData,
+      data: blogPost,
     });
   });
 };
 
-export const getById = (req, res) => {
-  fs.readFile(filepath, function (err, data) {
-    if (err && err.code == "ENOENT") {
-      return res.status(404).json({
-        status: 404,
-        message: "BlogPost for given id " + req.params.id + " Not Found",
-        data: {},
-      });
-    }
-    var blogposts = JSON.parse(data);
-    var blogpost = blogposts[req.params.id];
-    if (data.length == 0 || blogpost == undefined) {
+export const getById = async (req, res) => {
+  await getBlogPostByIdService(req.params.id).then((blogPost) => {
+    if (blogPost == undefined) {
       return res.status(404).json({
         status: 404,
         message: "BlogPost for given id " + req.params.id + " Not Found",
@@ -43,92 +40,50 @@ export const getById = (req, res) => {
     return res.status(201).json({
       status: 201,
       message: "Posts Fetched Successfully",
-      data: blogpost,
+      data: blogPost,
     });
   });
 };
 
-export const saveBlogPost = (req, res) => {
-  fs.readFile(filepath, (err, data) => {
-    var jsonData = {};
-    if (err && err.code == "ENOENT") {
-      const key = res.locals.blogpost.id;
-      const value = res.locals.blogpost;
-      jsonData[key] = value;
-    } else {
-      jsonData = JSON.parse(data);
-      jsonData[res.locals.blogpost.id] = res.locals.blogpost;
-    }
-
-    fs.writeFileSync(filepath, JSON.stringify(jsonData), (err) => {});
-
+export const saveBlogPost = async (req, res) => {
+  await saveBlogPostService(res.locals.blogPost).then((blogPost) => {
     return res.status(201).json({
       status: 201,
       message: "Post Inserted Successfully",
+      data: blogPost,
+    });
+  });
+};
+export const updateBlogPost = async (req, res) => {
+  await updateBlogPostService(res.locals.blogPost).then((blogPost) => {
+    if (blogPost == undefined) {
+      return res.status(404).json({
+        status: 404,
+        message: "BlogPost for given id " + req.params.id + " Not Found",
+        data: {},
+      });
+    }
+    return res.status(201).json({
+      status: 201,
+      message: "Post Updated Successfully",
       data: res.locals.blogpost,
     });
   });
 };
-export const updateBlogPost = (req, res) => {
-  fs.readFile(filepath, function (err, data) {
-    if (err && err.code == "ENOENT") {
+//delete
+export const deleteBlogPost = async (req, res) => {
+  await deleteBlogPostService(req.params.id).then((blogPost) => {
+    if (blogPost == undefined) {
       return res.status(404).json({
         status: 404,
         message: "BlogPost for given id " + req.params.id + " Not Found",
         data: {},
       });
-    }
-    data = JSON.parse(data);
-    var blogpost = data[res.locals.blogpost.id];
-    if (blogpost == undefined) {
-      return res.status(404).json({
-        status: 404,
-        message: "BlogPost for given id " + req.params.id + " Not Found",
-        data: {},
-      });
-    }
-    data[res.locals.blogpost.id] = res.locals.blogpost;
-
-    fs.writeFile(filepath, JSON.stringify(data), (err) => {
-      return res.status(201).json({
-        status: 201,
-        message: "Post Updated Successfully",
-        data: res.locals.blogpost,
-      });
-    });
-  });
-};
-
-export const deleteBlogPost = (req, res) => {
-  fs.readFile(filepath, function (err, data) {
-    if (err && err.code == "ENOENT") {
-      return res.status(404).json({
-        status: 404,
-        message: "BlogPost for given id " + req.params.id + " Not Found",
-        data: {},
-      });
-    }
-
-    data = JSON.parse(data);
-    var blogpost = data[req.params.id];
-
-    if (blogpost == undefined) {
-      return res.status(404).json({
-        status: 404,
-        message: "BlogPost for given id " + req.params.id + " Not Found",
-        data: {},
-      });
-    }
-    delete data[blogpost.id];
-    if (Object.keys(data).length == 0) {
-      fs.unlinkSync(filepath);
-    } else {
-      fs.writeFile(filepath, JSON.stringify(data), (err) => {});
     }
     return res.status(201).json({
       status: 201,
-      message: "BlogPost" + req.params.id + "is Deleted Successfully",
-      data: blogpost,
+      message: "Blog Post Deleted Successfully",
+      data: blogPost,
     });
   });
 };
---
+++ services/postservices.js
@@ -0,0 +1,102 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+var filepath = path.join(__dirname, "..", "jsondatastore/blogpost.json");
+//Get All Posts
+export const getAllBlogPostsService = () => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, "utf8", function (err, data) {
+      console.log(err);
+      if (err && err.code == "ENOENT") {
+        console.log("HERE");
+
+        resolve(undefined);
+      } else {
+        const jsonData = JSON.parse(data);
+        console.log("DATA PRINT");
+        console.log(jsonData);
+        resolve(jsonData);
+      }
+    });
+  });
+};
+//Get by id
+export const getBlogPostByIdService = (id) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, function (err, data) {
+      if (err && err.code == "ENOENT") {
+        return resolve(undefined);
+      }
+      var blogposts = JSON.parse(data);
+      var blogpost = blogposts[id];
+      if (data.length == 0 || blogpost == undefined) {
+        return resolve(undefined);
+      }
+      return resolve(blogpost);
+    });
+  });
+};
+export const deleteBlogPostService = (id) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, function (err, data) {
+      if (err && err.code == "ENOENT") {
+        return resolve(undefined);
+      }
+      data = JSON.parse(data);
+      var blogpost = data[id];
+
+      if (blogpost == undefined) {
+        return resolve(undefined);
+      }
+      delete data[blogpost.id];
+      if (Object.keys(data).length == 0) {
+        fs.unlinkSync(filepath);
+      } else {
+        fs.writeFile(filepath, JSON.stringify(data), (err) => {});
+      }
+      return resolve(blogpost);
+    });
+  });
+};
+//save blog post
+export const saveBlogPostService = (blogPost) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, (err, data) => {
+      var jsonData = {};
+      if (err && err.code == "ENOENT") {
+        const key = blogPost.id;
+        const value = blogPost;
+        jsonData[key] = value;
+      } else {
+        jsonData = JSON.parse(data);
+        jsonData[blogPost.id] = blogPost;
+      }
+
+      fs.writeFileSync(filepath, JSON.stringify(jsonData), (err) => {});
+
+      return resolve(blogPost);
+    });
+  });
+};
+export const updateBlogPostService = (blogPost) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filepath, function (err, data) {
+      if (err && err.code == "ENOENT") {
+        return resolve(undefined);
+      }
+      data = JSON.parse(data);
+      var blogpost = data[blogpost.id];
+      if (blogpost == undefined) {
+        return resolve(blogPost);
+      }
+      data[blogpost.id] = blogpost;
+
+      fs.writeFile(filepath, JSON.stringify(data), (err) => {
+        return resolve(blogPost);
+      });
+    });
+  });
+};
