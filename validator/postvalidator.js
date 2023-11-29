f5f08b78160f9beabf63b0d1e4c74e200e32f2ed
Pravin Nichal <nichal.pravin2001@gmail.com>
2023-11-29 09:56:57 +0530

    commit

--- validator/postvalidator.js
+++ validator/postvalidator.js
@@ -2,6 +2,27 @@ import { BlogPost } from "../models/blogpostmodel.js";
 import crypto from "crypto";
 export const postvalidator = (req, res, next) => {
   var blogPost = new BlogPost();
+  var validationErrors = {};
+  if (req.body.title == undefined) {
+    validationErrors["title"] = "Please Add Title";
+  }
+  if (req.body.author == undefined) {
+    validationErrors["author"] = "Please Add Author";
+  }
+  if (req.body.content == undefined) {
+    validationErrors["content"] = "Please Add Content";
+  }
+
+  if (Object.keys(validationErrors) != 0) {
+    return res
+      .status(403)
+      .json({
+        status: 403,
+        message: "Validation Error",
+        error: validationErrors,
+      });
+  }
+
   blogPost.title = req.body.title;
 
   if (req.originalUrl == "/blogpost/update") {
