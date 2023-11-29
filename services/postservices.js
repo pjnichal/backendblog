c318a47ea539745f43a295d9e2382369aa7e845d
Pravin Nichal <nichal.pravin2001@gmail.com>
2023-11-29 11:05:59 +0530

    commit

--- services/postservices.js
+++ services/postservices.js
@@ -1,6 +1,6 @@
 import fs from "fs";
 import { fileURLToPath } from "url";
-import path, { dirname, resolve } from "path";
+import path, { dirname } from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
